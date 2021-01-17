c6e9b6a89233453bfe0f89be7df522e4b50d4001
Everton Carneiro <everton.mercante@gmail.com>
2021-01-17 17:10:14 -0300

    Initial Commit

---
+++ Shared/GoFastApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoFastApp.swift
+//  Shared
+//
+//  Created by Everton Carneiro on 17/01/21.
+//
+
+import SwiftUI
+
+@main
+struct GoFastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
